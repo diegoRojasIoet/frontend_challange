cc19d77b2e2fb347211dda10dc91e38e0a46ad32
Diego Rojas <drojas@seatgeek.com>
2024-01-28 18:28:26 -0500

    feat: add get movie by id service

--- frontend/cinema/test/test_get_movies.test.js
+++ frontend/cinema/test/test_get_movies.test.js
@@ -1,7 +1,9 @@
 import { jest } from '@jest/globals'
 import { get_movies } from "../javascript/get-movies.js"
+import { get_movie_by_id } from "../javascript/get-movie.js"
 import { moviesResponse } from "./mocks/mock_movies.js"
-import { API_URL, OPTIONS } from "../javascript/constants.js"
+import { movieResponse } from "./mocks/mock_movie.js"
+import { API_URL, OPTIONS, API_MOVIE_ID_URL } from "../javascript/constants.js"
 
 
 describe('users service', () => {
@@ -17,5 +19,26 @@ describe('users service', () => {
     expect(fetch).toHaveBeenCalledTimes(1);
     expect(fetch).toHaveBeenCalledWith(`${API_URL}`, { "headers": { "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com", "X-RapidAPI-Key": "f4ccb234bamshd5a8e3e972f5eb3p1b55f1jsnf0aa770bcab3" }, "method": "GET" });
     expect(response).toEqual(JSON.parse(moviesResponse).results);
+  });
+  
+  it('get_movie should return a json with a movie', async () => {
+    const mockFetch = Promise.resolve({
+      text: () => Promise.resolve(movieResponse),
+    });
+    const ID = 'tt0055254'
+    const MOVIE_URL = API_MOVIE_ID_URL(ID)
+
+    console.log("mock")
+    console.log(JSON.parse(movieResponse).results)
+
+    global.fetch = jest.fn().mockImplementation(() => mockFetch);
+
+    const response = await get_movie_by_id(ID);
+    console.log("response")
+    console.log(response)
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(`${MOVIE_URL}`, { "headers": { "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com", "X-RapidAPI-Key": "f4ccb234bamshd5a8e3e972f5eb3p1b55f1jsnf0aa770bcab3" }, "method": "GET" });
+    expect(response).toEqual(JSON.parse(movieResponse).results);
   });
 });
