5865cb644ff5faa73d0d9b2d02c8c012ee1c4272
Diego Rojas <drojas@seatgeek.com>
2024-01-28 14:54:51 -0500

    feat: add test to get-movies

---
+++ frontend/cinema/test/mocks/mock_movies.js
@@ -0,0 +1,31 @@
+export const moviesResponse = `{
+        "results": 
+            [
+                { 
+                    "_id": "61e5811ce1a8ddbd96415553", 
+                    "id": "tt0082971", 
+                    "primaryImage":
+                        { 
+                            "id": "rm1612744448",
+                            "width": 1021,
+                            "height": 1536,
+                            "url": "https://m.media-amazon.com/images/M/MV5BMjA0ODEzMTc1Nl5BMl5BanBnXkFtZTcwODM2MjAxNA@@._V1_.jpg", 
+                            "caption": 
+                                { 
+                                    "plainText": "Harrison Ford in Raiders of the Lost Ark (1981)", 
+                                    "__typename": "Markdown" 
+                                }, 
+                                "__typename": "Image" 
+                        }, 
+                    "titleType": 
+                        { 
+                            "text": "Movie", 
+                            "id": "movie", 
+                            "isSeries": false, 
+                            "isEpisode": false, 
+                            "__typename": "TitleType" 
+                        }, 
+                    "titleText": { "text": "Indiana Jones and the Raiders of the Lost Ark", "__typename": "TitleText" }, "originalTitleText": { "text": "Raiders of the Lost Ark", "__typename": "TitleText" }, "releaseYear": { "year": 1981, "endYear": null, "__typename": "YearRange" }, "releaseDate": { "day": 13, "month": 8, "year": 1981, "__typename": "ReleaseDate" }, "position": 45 }, 
+                { "_id": "61e583cd8a5a6a599f41eaff", "id": "tt0325980", "primaryImage": { "id": "rm2487103488", "width": 671, "height": 1000, "url": "https://m.media-amazon.com/images/M/MV5BNGYyZGM5MGMtYTY2Ni00M2Y1LWIzNjQtYWUzM2VlNGVhMDNhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg", "caption": { "plainText": "Johnny Depp, Geoffrey Rush, Orlando Bloom, and Keira Knightley in Pirates of the Caribbean: The Curse of the Black Pearl (2003)", "__typename": "Markdown" }, "__typename": "Image" }, "titleType": { "text": "Movie", "id": "movie", "isSeries": false, "isEpisode": false, "__typename": "TitleType" }, "titleText": { "text": "Pirates of the Caribbean: The Curse of the Black Pearl", "__typename": "TitleText" }, "originalTitleText": { "text": "Pirates of the Caribbean: The Curse of the Black Pearl", "__typename": "TitleText" }, "releaseYear": { "year": 2003, "endYear": null, "__typename": "YearRange" }, "releaseDate": { "day": 14, "month": 8, "year": 2003, "__typename": "ReleaseDate" }, "position": 188 }
+            ] 
+    }`
