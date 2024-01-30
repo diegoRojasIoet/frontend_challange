5260d149e4a1f91bae6cab1e51ee541162328762
Diego Rojas <drojas@seatgeek.com>
2024-01-29 19:49:24 -0500

    feat: add rating

--- frontend/e-commerce/src/components/Filter/RatingFilter/Rating/index.jsx
+++ frontend/e-commerce/src/components/Filter/RatingFilter/Rating/index.jsx
@@ -3,6 +3,7 @@ import { AiOutlineStar } from 'react-icons/ai'
 import './Rating.css'
 
 function Rating ( {stars} ) {
+    debugger
     const fillStars = () => {
         const etiquetas = [];
         const starSelected = [...Array(stars)].map((_, index) => (
